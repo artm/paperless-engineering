62e355ad48802902c69f8be0147b09716144b72d
Artem Baguinski <artm@v2.nl>
2012-09-20 01:28:32 +0200

    implemented simulator

--- lib/simulator.rb
+++ lib/simulator.rb
@@ -1,11 +1,56 @@
 class Simulator
-  attr_reader :time
+  attr_reader :time, :events, :winner
+  There=1
+  Back=-1
 
-  def initialize
+  def initialize racer1, racer2, distance
+    @distance = distance
     @time=0
+    @winner=nil
+    @draw = false
+    @events = []
+    schedule racer1,0,There
+    schedule racer2,0,There
+    sort_events
   end
 
   def done?
-    false
+    @winner || @draw || @events.count == 0
+  end
+
+  def schedule racer, d, dir=1
+    leap, d_time = racer.simulate_leap
+    @events << { time: @time+d_time, racer: racer, d: d+leap*dir, dir: dir }
+  end
+
+  def sort_events
+    @events = @events.sort_by {|event| event[:time]}
+  end
+
+  def step
+    @time = @events[0][:time]
+    while @events.count>0 && @events[0][:time] == @time do
+      event = @events.shift
+      dir = event[:dir]
+      d = event[:d]
+      if d <= 0
+        if @winner
+          @winner = nil
+          @draw = true
+        else
+          @winner = event[:racer]
+        end
+      else
+        dir = Back if dir == There && d >= @distance
+        schedule event[:racer], d, dir
+        sort_events
+      end
+    end
+  end
+
+  def run
+    while !done?
+      step
+    end
   end
 end
--- spec/lib/simulator_spec.rb
+++ spec/lib/simulator_spec.rb
@@ -1,14 +1,47 @@
+require 'catdog'
 require 'simulator'
 
 describe Simulator do
   before do
-    @sim = Simulator.new
+    @cat = Animal.new 'Cat', 2, leap_time: 2
+    @dog = Animal.new 'Dog', 3, leap_time: 3
+    @sim = Simulator.new @dog, @cat, 1
   end
   it 'starts in a known state' do
     @sim.should_not be_done
     @sim.time.should == 0
   end
-  it 'schedules initial events'
-  it 'performs a step'
-  it 'knows when it is time to stop'
+  it 'schedules initial events' do
+    @sim.events[0][:time].should == 2
+    @sim.events[0][:racer].should == @cat
+    @sim.events[1][:time].should == 3
+    @sim.events[1][:racer].should == @dog
+  end
+  it 'performs a step' do
+    @sim.step
+    @sim.time.should == 2
+    @sim.events[0][:time].should == 3
+    @sim.events[0][:racer].should == @dog
+    @sim.events[1][:time].should == 4
+    @sim.events[1][:racer].should == @cat
+  end
+  it 'knows when it is time to stop' do
+    @sim.step
+    @sim.should_not be_done
+    @sim.step
+    @sim.should_not be_done
+    @sim.step
+    @sim.should be_done
+  end
+  it 'knows the winner' do
+    @sim.run
+    @sim.winner.should == @cat
+    winners = [nil,@cat,@cat,@dog,@cat,nil,nil,@cat,@cat]
+    (1..8).each do |d|
+      @sim = Simulator.new @dog, @cat, d
+      @sim.run
+      @sim.winner.should == winners[d]
+    end
+
+  end
 end
